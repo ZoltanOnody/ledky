4e12d2e9b10d7c8947db9a4d2806eb787f88a897
martinbaranek <36608613+martinbaranek@users.noreply.github.com>
2018-03-25 21:44:16 +0200

    Update bootscript.sh

--- bootscript.sh
+++ bootscript.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-source /home/martin/ledky/backend/venv/bin/activate
+WORKDIR=/home/martin/ledky/
+
+source "$WORKDIR"backend/venv/bin/activate
 fuser -k 8080/tcp  # will kill the app if it's already running
-FLASK_APP=/home/martin/ledky/backend/app.py nohup flask run --host=0.0.0.0 --port=8080 > /home/martin/ledky/backend/logs/stdout.log 2>/home/martin/ledky/backend/logs/stderr.log &
+FLASK_APP="$WORKDIR"backend/app.py nohup flask run --host=0.0.0.0 --port=8080 > "$WORKDIR"backend/logs/stdout.log 2>"$WORKDIR"backend/logs/stderr.log &
